cc8533b8bd2a7e2d2f2ab7a29405904ba96bde6c
pornima0306 <pornimawaghmare0306@gmail.com>
2022-07-18 18:06:55 +0530

    2Array Data in Table

---
+++ assets/js/app.js
@@ -0,0 +1,330 @@
+let cl = console.log;
+
+let array1 = [
+    {
+        "id": 1,
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+        }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+        "name": "Romaguera-Crona",
+        "catchPhrase": "Multi-layered client-server neural-net",
+        "bs": "harness real-time e-markets"
+        }
+        },
+        {
+        "id": 2,
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+        "street": "Victor Plains",
+        "suite": "Suite 879",
+        "city": "Wisokyburgh",
+        "zipcode": "90566-7771",
+        "geo": {
+        "lat": "-43.9509",
+        "lng": "-34.4618"
+        }
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+        "name": "Deckow-Crist",
+        "catchPhrase": "Proactive didactic contingency",
+        "bs": "synergize scalable supply-chains"
+        }
+        },
+        {
+        "id": 3,
+        "username": "Samantha",
+        "email": "Nathan@yesenia.net",
+        "address": {
+        "street": "Douglas Extension",
+        "suite": "Suite 847",
+        "city": "McKenziehaven",
+        "zipcode": "59590-4157",
+        "geo": {
+        "lat": "-68.6102",
+        "lng": "-47.0653"
+        }
+        },
+        "phone": "1-463-123-4447",
+        "website": "ramiro.info",
+        "company": {
+        "name": "Romaguera-Jacobson",
+        "catchPhrase": "Face to face bifurcated interface",
+        "bs": "e-enable strategic applications"
+        }
+        },
+        {
+        "id": 4,
+        "username": "Karianne",
+        "email": "Julianne.OConner@kory.org",
+        "address": {
+        "street": "Hoeger Mall",
+        "suite": "Apt. 692",
+        "city": "South Elvis",
+        "zipcode": "53919-4257",
+        "geo": {
+        "lat": "29.4572",
+        "lng": "-164.2990"
+        }
+        },
+        "phone": "493-170-9623 x156",
+        "website": "kale.biz",
+        "company": {
+        "name": "Robel-Corkery",
+        "catchPhrase": "Multi-tiered zero tolerance productivity",
+        "bs": "transition cutting-edge web services"
+        }
+        },
+        {
+        "id": 5,
+        "address": {
+        "street": "Skiles Walks",
+        "suite": "Suite 351",
+        "city": "Roscoeview",
+        "zipcode": "33263",
+        "geo": {
+        "lat": "-31.8129",
+        "lng": "62.5342"
+        }
+        },
+        "phone": "(254)954-1289",
+        "website": "demarco.info",
+        "company": {
+        "name": "Keebler LLC",
+        "catchPhrase": "User-centric fault-tolerant solution",
+        "bs": "revolutionize end-to-end systems"
+        }
+        },
+        {
+        "id": 6,
+        "username": "Leopoldo_Corkery",
+        "email": "Karley_Dach@jasper.info",
+        "address": {
+        "street": "Norberto Crossing",
+        "suite": "Apt. 950",
+        "city": "South Christy",
+        "zipcode": "23505-1337",
+        "geo": {
+        "lat": "-71.4197",
+        "lng": "71.7478"
+        }
+        },
+        "phone": "1-477-935-8478 x6430",
+        "website": "ola.org",
+        "company": {
+        "name": "Considine-Lockman",
+        "catchPhrase": "Synchronised bottom-line interface",
+        "bs": "e-enable innovative applications"
+        }
+        },
+        {
+        "id": 7,
+        "name":"Kurtis Weissanat",
+        "username": "Elwyn.Skiles",
+        "email": "Telly.Hoeger@billy.biz",
+        "address": {
+        "street": "Rex Trail",
+        "suite": "Suite 280",
+        "city": "Howemouth",
+        "zipcode": "58804-1099",
+        "geo": {
+        "lat": "24.8918",
+        "lng": "21.8984"
+        }
+        },
+        "phone": "210.067.6132",
+        "website": "elvis.io",
+        "company": {
+        "name": "Johns Group",
+        "catchPhrase": "Configurable multimedia task-force",
+        "bs": "generate enterprise e-tailers"
+        }
+        },
+        {
+        "id": 8,
+        "address": {
+        "street": "Ellsworth Summit",
+        "suite": "Suite 729",
+        "city": "Aliyaview",
+        "zipcode": "45169",
+        "geo": {
+        "lat": "-14.3990",
+        "lng": "-120.7677"
+        }
+        },
+        "phone": "586.493.6943 x140",
+        "website": "jacynthe.com",
+        "company": {
+        "name": "Abernathy Group",
+        "catchPhrase": "Implemented secondary concept",
+        "bs": "e-enable extensible e-tailers"
+        }
+        },
+        {
+        "id": 9,
+        "username": "Delphine",
+        "email": "Chaim_McDermott@dana.io",
+        "address": {
+        "street": "Dayna Park",
+        "suite": "Suite 449",
+        "city": "Bartholomebury",
+        "zipcode": "76495-3109",
+        "geo": {
+        "lat": "24.6463",
+        "lng": "-168.8889"
+        }
+        },
+        "phone": "(775)976-6794 x41206",
+        "website": "conrad.com",
+        "company": {
+        "name": "Yost and Sons",
+        "catchPhrase": "Switchable contextually-based project",
+        "bs": "aggregate real-time technologies"
+        }
+        },
+        {
+        "id": 10,
+        "username": "Moriah.Stanton",
+        "email": "Rey.Padberg@karina.biz",
+        "address": {
+        "street": "Kattie Turnpike",
+        "suite": "Suite 198",
+        "city": "Lebsackbury",
+        "zipcode": "31428-2261",
+        "geo": {
+        "lat": "-38.2386",
+        "lng": "57.2232"
+        }
+        },
+        "phone": "024-648-3804",
+        "website": "ambrose.net",
+        "company": {
+        "name": "Hoeger LLC",
+        "catchPhrase": "Centralized empowering task-force",
+        "bs": "target end-to-end models"
+        }
+        }
+]
+
+let array2 = [{
+    "id": 1,
+    "name": "Leanne Graham3",  
+    },
+    {  
+    "id": 2,
+    "name": "Ervin Howell"
+    },
+    {
+    "id": 3,
+    "name": "Clementine Bauch"
+    },
+    {
+    "id": 4,
+    "name": "Patricia Lebsack"
+    },
+    {
+    "id": 5,
+    "name": "Chelsey Dietrich",
+    },
+    
+    {
+    "id": 6,
+    "name": "Mrs. Dennis Schulist"
+    },
+    {
+    "id": 7,
+    "name": "Kurtis Weissnat"
+    },
+    {
+    "id": 9,
+    "name": ""
+    }
+    ]
+    cl(array2[1].id);
+
+/* let newArr = array2.map(ele =>{
+        if(!ele.name){
+             ele.name = array1.username;
+             ele.status = 'unAssigned'
+            return ele
+        }else{
+            ele.status = 'UserName is Assigned'
+
+            return ele;
+
+        }
+    })
+
+    cl(newArr) */
+//     You have to create a table in which you have to
+//  1. get the names by using the id from the 2nd array,
+//  2. if the name is empty then that user is "unassigned",
+//  3. If the name is missing in the 2nd array for the id then you have to use the username
+
+let data = document.getElementById('data');
+
+
+
+array3=[];
+array2.forEach(e=>{
+    array3.push(e.id)
+})
+cl(array3)
+
+
+function tableDataOperations(arr1,arr2,arr3){
+    arr1.forEach(ele=>{
+        if(arr3.includes(ele.id)){
+            if(ele.name ="undefined"){
+            
+                arr2.forEach(element =>{
+                    if(element.id===ele.id){
+                        ele.name = element.name;
+                        ele.status = "Assigned"
+                    }
+                    if((element.id===ele.id) && !ele.name){
+                        ele.name = ele.username;
+                    }   
+                })
+            }  
+        }else{
+            ele.name ="User is Deleted";
+            ele.status = "Unassigned";
+        }
+    })    
+}
+
+tableDataOperations(array1,array2,array3)
+
+function templating(arr){
+    let result = '';
+
+    arr.forEach(ele =>{
+        result += `<tr>
+            <td>${ele.id}</td>
+            <td>${ele.name}</td>
+            <td>${ele.status}</td>
+        </tr>`
+    })
+    data.innerHTML = result;
+}
+templating(array1);
+
+
+
+
+
